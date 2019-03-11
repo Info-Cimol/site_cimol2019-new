a74f0f038314026acc3a1708b82520053859df12
Info-Cimol <info.cimol@gmail.com>
2019-03-11 11:09:58 -0300

    Upload-publicAdmin

---
+++ admin/content.php
@@ -0,0 +1,10 @@
+<div class="span9">
+	
+		
+	<?php echo 1?>	
+
+	
+    <div class="content" id="ConteudoAqui">
+    	<?php
+    	require_once($this->config->item("base_dir").'admin/view/'.$content.".php");
+    	?>
