51fa8e4b7a9f27cf6c27a548c46fbe8c392ec54f
Info-Cimol <info.cimol@gmail.com>
2019-03-18 15:19:11 -0300

    noticia (JSON_image)

--- public/plugins/tiny_mce/lists/image_list.js
+++ public/plugins/tiny_mce/lists/image_list.js
@@ -5,5 +5,6 @@
 var tinyMCEImageList = new Array(
 	// Name, URL
 	["Logo 1", "media/logo.jpg"],
-	["Logo 2 Over", "media/logo_over.jpg"]
+	["Logo 2 Over", "media/logo_over.jpg"],
+	["teste nome", "teste url"]
 );
